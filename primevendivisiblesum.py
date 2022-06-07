08e15c4c49b176921440b7bb0feb3d63a2d4cf94
KethuBhargavReddy <79352407+kbhargavreddy@users.noreply.github.com>
2022-06-07 15:06:03 +0530

    Update primevendivisiblesum.py

--- primevendivisiblesum.py
+++ primevendivisiblesum.py
@@ -1,19 +1,17 @@
 """ Check the number is prime or not, even or not, divisible by 5 not, sum of digits """
-
-n=int(input("Enter a number"))
-if(n%5==0):
-  print("It is divisible by 5")
-else:
-  print("Not divisible by 5")
-if(n%2==0):
-  print("It is even")
-else:
-  print("It is odd")
+n=int(input())
 for i in range(2,int(n**0.5)+1):
-  if(n%i==0):
-    print("It is not a prime")
-    break
+    if(n%i==0):
+        print("not prime")
+        break
 else:
-  print("It is a prime")
-sum=(n*(n+1))/2
-print(sum)
+    print("prime")
+if(n%2==0):
+    print("even")
+else:
+    print("odd")
+if(str(n)[-1]=='0' or str(n)[-1]=='5'):
+    print("yes")
+else:
+    print("no")
+print("the sum is",(n*(n+1))//2)
