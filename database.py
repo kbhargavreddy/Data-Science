c772c0b81b859663cfd833d579517dc23f329a54
KethuBhargavReddy <79352407+kbhargavreddy@users.noreply.github.com>
2022-06-07 15:04:54 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,27 @@
 """ Create a database having employess data """
-a=dict()
-a[1]=[101,'sree',9.8,900,7.2,'python','marriage']
-a[2]=[102,'ajay',7.6,920,6.9,'python','own company']
-a[3]=[103,'akhil',10,937,8.2,'python','masters']
-print(a)
+ans=[]
+for i in range(2):
+    l=[]
+    id1=int(input("enter id"))
+    name=input("enter name")
+    marks_ssc=int(input("ssc marks"))
+    marks_intermediate=int(input("inter marks"))
+    cgpa=float(input("btech cgpa"))
+    lang=input("preffered language")
+    career=input("career choice")
+    l.append(id1)
+    l.append(name)
+    l.append(marks_ssc)
+    l.append(marks_intermediate)
+    l.append(cgpa)
+    l.append(lang)
+    l.append(career)
+    ans.append(l)
+print(ans)
+
+
+#using dictionary
+d=dict()
+d[0]={101,'bhargav',980,978,'pyhton','data science'}
+d[1]={102,'saatvik',940,928,'pyhton','web tech'}
+d[2]={103,'lakshma',1000,1000,'all lang','everything'}
